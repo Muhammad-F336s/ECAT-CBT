12ff45ca48dfcf3d5411dddf545836be62ac24a7
Hafiz Muhammad Faizan <Muhammad.F336s@gmail.com>
2026-06-22 23:26:03 +0500

    Started Project by creating Vite app

---
+++ Project-Palyground/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        className="
+  "
+      >
+        Hi this is CBT main entry point
+      </div>
+    </>
+  );
+}
+
+export default App;
